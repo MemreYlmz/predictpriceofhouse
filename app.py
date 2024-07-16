6e1230a158ca281946739b3c5ce7bce47af4ccce
Mehmet_Emre Yılmaz <mehmetemreyilmaz199991@gmail.com>
2024-07-16 22:45:22 +0300

    predict done

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
 import sqlite3
 import pandas as pd
-import joblib
-
+import pickle
 
 app = Flask(__name__)
 
 # Model ve pipeline'ı yükleme
-model = joblib.load('model.pkl')
-full_pipeline = joblib.load("pipeline.pkl")
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
-
+with open('pipeline.pkl', 'rb') as f:
+    full_pipeline = pickle.load(f)
 
 def get_db_connection():
     conn = sqlite3.connect('hepsiemlak.db')
@@ -24,60 +24,19 @@ def get_emlak():
     neighborhood = request.args.get('neighborhood')
 
     conn = get_db_connection()
-    query = 'SELECT * FROM emlak_verileri WHERE city=?'
-    params = [city]
-
-    if district:
-        query += ' AND district=?'
-        params.append(district)
-    if neighborhood:
-        query += ' AND neighborhood=?'
-        params.append(neighborhood)
-
-    emlak = conn.execute(query, params).fetchall()
+    query = 'SELECT * FROM emlak_verileri WHERE city=? AND district=? AND neighborhood=?'
+    emlak = conn.execute(query, (city, district, neighborhood)).fetchall()
     conn.close()
 
     emlak_list = [dict(ix) for ix in emlak]
     return jsonify(emlak_list)
 
-##@app.route('/emlak_district', methods=['GET'])
-##def get_emlak_district():
-##    city = request.args.get('city')
-##    district = request.args.get('district')
-##    conn = get_db_connection()
-##    query = 'SELECT * FROM emlak_verileri WHERE city=? AND district=? '
-##    emlak = conn.execute(query, (city, district)).fetchall()
-##    conn.close()
-##    emlak_list = [dict(ix) for ix in emlak]
-##    return jsonify(emlak_list)
-##
-##
-##
-##@app.route('/emlak_detail', methods=['GET'])
-##def get_emlak_detail():
-##    city = request.args.get('city')
-##    district = request.args.get('district')
-##    neighborhood = request.args.get('neighborhood')
-##
-##    conn = get_db_connection()
-##    query = 'SELECT * FROM emlak_verileri WHERE city=? AND district=? AND neighborhood=? '
-##    emlak = conn.execute(query, (city, district,neighborhood)).fetchall()
-##    conn.close()
-##    emlak_list_detail = [dict(ix) for ix in emlak]
-##    return jsonify(emlak_list_detail)
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    print("Received data:", data)
     df = pd.DataFrame([data])
-    print("DataFrame:\n", df)
-    print(df.info())
     prepared_data = full_pipeline.transform(df)
-    print("Prepared data:\n", prepared_data)
-    print(prepared_data.shape)
-    prediction = model.predict(prepared_data)
-    print("Prediction:", prediction)
+    prediction = model.named_steps["model"].predict(prepared_data) #named_steps önemli
     return jsonify({'price': prediction[0]})
 
 if __name__ == '__main__':
--- data_prepratation.py
+++ data_prepratation.py
@@ -1,81 +1,15 @@
-#import pandas as pd
-#import sqlite3
-#import joblib
-#from sklearn.model_selection import train_test_split
-#from sklearn.pipeline import Pipeline
-#from sklearn.preprocessing import StandardScaler, OneHotEncoder
-#from sklearn.compose import ColumnTransformer
-#from sklearn.ensemble import RandomForestClassifier
-#
-#
-#
-## Veriyi yükleme ve temizleme
-#df = pd.read_csv("data_cleaned2.csv")
-#df["city"] = df["city"].astype("category")
-#df["district"] = df["district"].astype("category")
-#df["neighborhood"] = df["neighborhood"].astype("category")
-#df["room"] = df["room"].astype("int")
-#df["living_room"] = df["living_room"].astype("int")
-#df["area"] = df["area"].astype("int")
-#df["age"] = df["age"].astype("int")
-#df["floor"] = df["floor"].astype("int")
-#df["price"] = df["price"].astype("int")
-#
-## Pipeline oluşturma
-#categorical_features = ["city", "district", "neighborhood"]
-#numerical_features = ["room", "living_room", "area", "age", "floor"]
-#full_pipeline = ColumnTransformer([
-#    ("num", StandardScaler(), numerical_features),
-#    ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features)
-#])
-#
-## Model eğitme
-#X = df.drop("price", axis=1)
-#y = df["price"]
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#model = Pipeline([
-#    ("preparation", full_pipeline),
-#    ("model", RandomForestClassifier(n_estimators=100))
-#])
-#model.fit(X_train, y_train)
-#
-#
-#new_data = pd.DataFrame({
-#    "city":["İzmir"],
-#    "district":["bornova"],
-#    "neighborhood":["erzene"],
-#    "room":[5],
-#    "living_room":[1],
-#    "area":[80],
-#    "age":[5],
-#    "floor":[3],
-#})
-#print(model.predict(new_data))
-#print(df[(df["city"]=="izmir")&(df["district"]=="bornova")&(df["neighborhood"]=="erzene")])
-#
-## Model ve pipeline'ı kaydetme
-#joblib.dump(model, 'model.pkl')
-#joblib.dump(full_pipeline, 'pipeline.pkl')
-#
-## Veritabanına kaydetme
-#conn = sqlite3.connect('hepsiemlak.db')
-#df.to_sql('emlak_verileri', conn, if_exists='replace', index=False)
-#conn.close()
-
-
-#DENEME
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 import sqlite3
-import joblib
+import pickle
+
+# Veriyi yükleme ve ön işleme
 df = pd.read_csv("data_cleaned2.csv")
 
 df["city"] = df["city"].astype("category")
@@ -88,53 +22,51 @@ df["age"] = df["age"].astype("int")
 df["floor"] = df["floor"].astype("int")
 df["price"] = df["price"].astype("int")
 
-categorical_features = ["city","district","neighborhood"]
-numerical_features = ["room","living_room","area","age","floor"]
+categorical_features = ["city", "district", "neighborhood"]
+numerical_features = ["room", "living_room", "area", "age", "floor"]
 
 full_pipeline = ColumnTransformer([
-    ("num",StandardScaler(),numerical_features),
-    ("cat",OneHotEncoder(handle_unknown="ignore"),categorical_features)
+    ("num", StandardScaler(), numerical_features),
+    ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features)
 ])
 
-X = df.drop("price",axis=1)
+X = df.drop("price", axis=1)
 y = df["price"]
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = Pipeline([
-    ("preparation",full_pipeline),
-    ("model",LinearRegression())
+    ("preparation", full_pipeline),
+    ("model", LinearRegression())
 ])
 
-model.fit(X_train,y_train)
+model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-mse = mean_squared_error(y_test ,y_pred)
+mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
-r2=r2_score(y_test,y_pred)
+r2 = r2_score(y_test, y_pred)
 
 new_data = pd.DataFrame({
-    "city":["İzmir"],
-    "district":["karabağlar"],
-    "neighborhood":["refet_bele"],
-    "room":[3],
-    "living_room":[1],
-    "area":[120],
-    "age":[0],
-    "floor":[3],
+    "city": ["İzmir"],
+    "district": ["karabağlar"],
+    "neighborhood": ["refet_bele"],
+    "room": [3],
+    "living_room": [1],
+    "area": [120],
+    "age": [0],
+    "floor": [3],
 })
 print(model.predict(new_data))
 
+# Model ve pipeline'ı kaydetme
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
 
-## Model ve pipeline'ı kaydetme
-joblib.dump(model, 'model.pkl')
-joblib.dump(full_pipeline, 'pipeline.pkl')
+with open('pipeline.pkl', 'wb') as f:
+    pickle.dump(full_pipeline, f)
 
 # Veritabanına kaydetme
 conn = sqlite3.connect('hepsiemlak.db')
 df.to_sql('emlak_verileri', conn, if_exists='replace', index=False)
 conn.close()
-
-
-
-
